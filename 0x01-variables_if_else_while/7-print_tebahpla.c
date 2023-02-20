5820237c98489132960c3b8effa37e2aa10a50b5
mszhdgr <smawali876@gmail.com>
2023-02-20 09:04:17 -0800

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main -a program that prints rev alphabets in lowercase
+ *
+ * Return: (0) (successful)
+ */
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+	putchar(letter);
+	letter++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
