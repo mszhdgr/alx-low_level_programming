00692a44975d5a3da53ee580dd00046eca23606c
mszhdgr <smawali876@gmail.com>
2023-02-22 01:15:18 -0800

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,14 +8,15 @@
 void print_alphabet_x10(void)
 {
 	char c;
-	int i;
+	int i = 0;
 
-	for (i = 0; i < 10; i++)
+	while (i <= 10)
 	{
 	for (c = 'a'; c <= 'z'; c++)
 	{
 	_putchar(c);
 	}
 	_putchar('\n');
+	i++;
 	}
 }
