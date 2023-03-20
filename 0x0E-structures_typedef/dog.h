060c22cca459a618f00f4e80f0d0a3822b6a6c68
mszhdgr <smawali876@gmail.com>
2023-03-20 02:03:14 -0700

    Tasks 0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+/**
+ * struct dog - it is a structure that stores dogs info
+ *
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ *
+ */
+struct dog
+{
+
+char *name[20];
+float age;
+char *owner[20];
+
+}
+#endif
