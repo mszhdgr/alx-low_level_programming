a1636c435c58b8ea782c2e612462b8ce1d09959d
mszhdgr <smawali876@gmail.com>
2023-03-31 06:37:32 -0700

    Tasks 1. List length

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,22 +1,20 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include "lists.h"
+
 /**
- * list_len -  a function that returns the number
- * of elements in a linked list_t
+ * list_len - returns the number of elements in a linked list
+ * @h: pointer to the list_t list
  *
- * @h: pointer to the first node
- *
- * Return: s
+ * Return: number of elements in h
  */
 size_t list_len(const list_t *h)
 {
-	size_t s = 0;
+	size_t n = 0;
 
-	while (h != NULL)
+	while (h)
 	{
-	s++;
-	h = h->next;
+		n++;
+		h = h->next;
 	}
-
-	return (s);
+	return (n);
 }
