9b3644490e9b54ed87a7b294517e61452c0684fe
mszhdgr <smwavali876@gmail.com>
2023-04-10 12:54:52 -0700

    tasks

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,54 @@
+#include "main.h"
+#include <stdarg.h>
+/**
+ * main - a program that copies the content of a file to
+ * another file.
+ *
+ * @argc: the no. of arguments passed to the program
+ * @argv: the array of arguments to be passed
+ *
+ * Return: 0 on (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int fd_write, fd_read, a, b, c;
+	char buffer[BUFSIZ];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	fd_read = open(argv[1], O_RDONLY);
+	if (fd_read < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	fd_write = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	while ((a = read(fd_read, buffer, BUFSIZ)) > 0)
+	{
+		if (fd_write < 0 || write(fd_write, buffer, a) != a)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			close(fd_read);
+			exit(99);
+		}
+	}
+	if (a < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	b = close(fd_read);
+	c = close(fd_write);
+	if (b < 0 || c < 0)
+	{
+		if (b < 0)
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_read);
+		if (c < 0)
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_write);
+		exit(100);
+	}
+	return (0);
+}
