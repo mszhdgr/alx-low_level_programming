8d5c686d7d613499c387dc29deb939618679cac2
mszhdgr <smwavali876@gmail.com>
2023-04-10 11:27:47 -0700

    tasks

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -5,35 +5,38 @@
  * prints it to the POSIX standard output.
  *
  * @filename: a pointer to the file to be read
- * @letters: letters is the number of letters it should read and print
+ * @letters: is the number of letters it should read and print
  *
  * Return: if write fails return 0
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t input_fd, readed, writ;
-	char *read_buffer;
+	int fd;
+	int m, x;
+	char *buf;
 
-	if (filename == NULL)
+	if (!filename)
 		return (0);
-
-	read_buffer = malloc(sizeof(char) * letters);
-	if (read_buffer == NULL)
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
 		return (0);
-
-	input_fd = open(filename, O_RDONLY);
-	readed = read(input_fd, read_buffer, letters);
-	writ = write(STDOUT_FILENO, read_buffer, readed);
-
-	if (input_fd == -1 || readed == -1 || writ == -1 || writ != readed)
+	buf = malloc(sizeof(char) * letters);
+	if (!buf)
+		return (0);
+	m = read(fd, buf, letters);
+	if (m < 0)
 	{
-		free(read_buffer);
+		free(buf);
 		return (0);
 	}
-
-	free(read_buffer);
-	close(input_fd);
-
-	return (writ);
+	buf[m] = '\0';
+	close(fd);
+	x = write(STDOUT_FILENO, buf, m);
+	if (x < 0)
+	{
+		free(buf);
+		return (0);
+	}
+	free(buf);
+	return (x);
 }
